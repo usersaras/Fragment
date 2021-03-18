98c71f891009f8d3a99f96fcfeb6c0ff060ea2ff
usersaras <saras.sthapitshrestha@gmail.com>
2021-03-18 09:46:51 +0545

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+FRAGMENT- FragmentExample2 <br>
+![GIF of FragmentExample2 Application 2](./FragmentExample2.gif) <br>
+
---
+++ app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
@@ -0,0 +1,72 @@
+
+
+package com.example.android.fragmentexample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+
+public class SimpleFragment extends Fragment {
+
+    // The radio button choice has 3 states: 0 = yes, 1 = no,
+    // 2 = default (no choice). Using only 0 and 1.
+    private static final int YES = 0;
+    private static final int NO = 1;
+
+    public SimpleFragment() {
+
+    }
+
+    /**
+     * Creates the view for the fragment.
+     *
+     * @param inflater           LayoutInflater to inflate any views in the fragment
+     * @param container          ViewGroup of parent view to attach fragment
+     * @param savedInstanceState Bundle for previous state
+     * @return rootView
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment.
+        final View rootView = inflater.inflate(R.layout.fragment_simple,
+                container, false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+
+        // Set the radioGroup onCheckedChanged listener.
+        radioGroup.setOnCheckedChangeListener(
+                new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        View radioButton = radioGroup.findViewById(checkedId);
+                        int index = radioGroup.indexOfChild(radioButton);
+                        TextView textView =
+                                rootView.findViewById(R.id.fragment_header);
+                        switch (index) {
+                            case YES: // User chose "Yes".
+                                textView.setText(R.string.yes_message);
+                                break;
+                            case NO: // User chose "No".
+                                textView.setText(R.string.no_message);
+                                break;
+                            default: // No choice made.
+                                // Do nothing.
+                                break;
+                        }
+                    }
+                });
+
+        // Return the View for the fragment's UI.
+        return rootView;
+    }
+
+    public static SimpleFragment newInstance() {
+        return new SimpleFragment();
+    }
+}
